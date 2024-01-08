104844963dd69f2fa685b74c9db8f18329a10e5f
Lorenz Müller <muellerlorenz64@gmail.com>
2024-01-08 20:10:20 +0100

    add calculator aufgabe

---
+++ Aufgaben/Aufgabe3/Calculator/src/test/CalculatorTest.java
@@ -0,0 +1,31 @@
+package Aufgabe3.Calculator.src.test;
+
+import Aufgabe3.Calculator.src.main.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void add() {
+        assertEquals(105.2, calculator.add(26.3, 78.9));
+    }
+
+    @Test
+    void remove() {
+        assertEquals(21, calculator.remove(31, 10));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(310, calculator.multiply(31, 10));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(5, calculator.divide(25, 5));
+    }
+}
